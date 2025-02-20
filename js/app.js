9692fc57b21913058bb6a4df6077c7d69fa3ff15
Abdulloh Nurmukhammedov <158317198+AbdullohwebS@users.noreply.github.com>
2025-02-20 16:10:44 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const darkModeToggle = document.querySelector(".icon img");
 const body = document.body;
 
-const darkImg = "images/iconics-image/Dark mode.png";
+const darkImg = "Dark mode(1).png";
 const darkImg = "images/iconics-image/Dark mode.png";
 
 if (localStorage.getItem("theme") === "light") {
