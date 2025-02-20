22b27c2d3a450a054c3b8294fce85e25330be9a8
AbdullohwebS <abdullohnurmuhammedov654@gmail.com>
2025-02-20 16:18:44 +0500

    fix

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const darkModeToggle = document.querySelector(".icon img");
 const body = document.body;
 
+const lightImg = "images/logotip/Dark mode (1).png";
 const darkImg = "images/iconics-image/Dark mode.png";
-const lightImg = "images/iconics-image/Dark mode.png";
 
 if (localStorage.getItem("theme") === "light") {
   body.classList.add("light-mode");
